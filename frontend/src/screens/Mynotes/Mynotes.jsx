17264a49275775da768ceacd51737aa89319ce01
Chaitrali kakde <96014425+charu1603@users.noreply.github.com>
2023-04-29 09:52:45 +0530

    Delete frontend directory

--- frontend/src/screens/Mynotes/Mynotes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Mynotes = () => {
-  return (
-    <div>Mynotes</div>
-  )
-}
-
-export default Mynotes
--- frontend/src/screens/Singlenote/Singlenote.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Singlenote = () => {
-  return (
-    <div>Singlenote</div>
-  )
-}
-
-export default Singlenote
